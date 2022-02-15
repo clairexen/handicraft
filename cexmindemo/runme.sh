e297465e2fc13918c329a5cc182cc920c1a7ba89
Claire Xenia Wolf <claire@clairexen.net>
2022-02-15 16:42:38 +0100

    Update
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- cexmindemo/runme.sh
+++ cexmindemo/runme.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -x
 sby -f abc_bmc3.sby
-cexoptions="ac anc anmc amc amuc amec amoc amxc"
+cexoptions="c nc ac anc anmc amc amuc amec amoc amxc"
 yosys-abc -c 'read_aiger abc_bmc3/model/design_aiger.aig; fold; strash; bmc3 -F 20 -v' \
     -c "$(for o in $cexoptions; do echo write_cex -$o trace_$o.aiw\;; done;)"
-cat trace_ac.aiw
-cat trace_amc.aiw
-cat trace_amuc.aiw
-cat trace_amxc.aiw
+paste trace_ac.aiw trace_amc.aiw trace_amuc.aiw trace_amec.aiw trace_amoc.aiw trace_amxc.aiw | expand -t20
 exit 0
