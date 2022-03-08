12d45a5a39dcb856cd13b34c618e727527c6a4c5
Claire Xenia Wolf <claire@clairexen.net>
2022-03-08 20:39:34 +0100

    ...
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- mddbench/scripts/manifest.py
+++ mddbench/scripts/manifest.py
@@ -1,4 +1,4 @@
-import yaml
+import yaml, os
 from utils import *
 
 with open("config.yaml") as cfg:
@@ -12,4 +12,23 @@ for name, args in config["benchmarks"].items():
             assert key not in manifest
             manifest[key] = val
 
-print(yaml.dump(manifest))
+os.makedirs("output", exist_ok=True)
+with open("output/manifest.mk", "w") as f:
+    for bench, tools in manifest.items():
+        os.makedirs(f"output/{bench}", exist_ok=True)
+        print(f"output/{bench}/benchmark.ok:", file=f)
+        print(f"\t@echo 'Setting up benchmark {bench}.'", file=f)
+        print(f"\t@touch output/{bench}/benchmark.ok", file=f)
+        print("", file=f)
+
+        for tool, dep in tools.items():
+            os.makedirs(f"output/{bench}/datapoint-{tool}", exist_ok=True)
+            if dep is None:
+                dep = f"output/{bench}/benchmark.ok"
+            else:
+                dep = f"output/{bench}/datapoint-{dep}/datapoint.ok"
+            print(f"datapoints:: output/{bench}/datapoint-{tool}/datapoint.ok", file=f)
+            print(f"output/{bench}/datapoint-{tool}/datapoint.ok: {dep}", file=f)
+            print(f"\t@echo 'Running benchmark {bench} through {tool}.'", file=f)
+            print(f"\t@touch output/{bench}/datapoint-{tool}/datapoint.ok", file=f)
+            print("", file=f)
