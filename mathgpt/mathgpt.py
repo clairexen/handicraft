9cdc28c6aa0705911e66621ab636ee7d39d2f8a2
Claire Xenia Wolf <claire@clairexen.net>
2023-03-15 10:54:10 +0100

    ...
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- mathgpt/mathgpt.py
+++ mathgpt/mathgpt.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+#
+# MathGPT -- A math engine based on GPT
+#
+# Copyright (C) 2023 Claire Xenia Wolf <claire@yosyshq.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
 
 import openai, os
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -28,10 +45,52 @@ def complete(*args, **kwargs):
     # pp(data)
     return data[2]["choices"][0]["text"]
 
+def evalExprStep(expr):
+    def getNumber(token):
+        if type(token) is float:
+            return token
+        if re.match(r"^\.?[0-9]+(?:\.[0-9]+)?(?:e[-+]?[0-9]+)?$", token):
+            return float(token)
+        return None
+
+    def getOperator(ops):
+        def f(token):
+            if type(token) is str and token in ops:
+                return token
+            return None
+        return f
+
+    def update(pattern, func):
+        cursor = 0
+        newExpr = []
+        foundMatch = False
+        while cursor+len(pattern) <= len(expr):
+            values = [pattern[i](expr[cursor+i]) for i in range(len(pattern))]
+            if None in values:
+                newExpr.append(expr[cursor])
+                cursor += 1
+            else:
+                newExpr.append(func(*values))
+                cursor += len(pattern)
+                foundMatch = True
+        if foundMatch:
+            return newExpr
+        return None
+
+    if newExpr := update([getNumber, getOperator("*/"), getNumber], lambda a,b,c: a*c if b == "*" else a/c):
+        return newExpr
+
+    if newExpr := update([getNumber, getOperator("+-"), getNumber], lambda a,b,c: a+c if b == "+" else a-c):
+        return newExpr
+
+    return expr
 
 class MathGPT:
     def __init__(self, text):
         self.text = text
+        self.state = dict()
+        self.nextState = dict()
+        self.varNames = list()
 
     def getTrainingJSONL(self):
         data = list()
@@ -59,6 +118,60 @@ class MathGPT:
             while cursor < len(txt) and txt[cursor-1] != "\n": cursor += 1
 
         return "\n".join([f'{{ "prompt": {json.dumps(p)}, "completion": {json.dumps(c)} }}' for p, c in data])
+
+    def appendText(self, text):
+        self.text += text
+        return text
+
+    def appendOutput(self):
+        command = self.text.rsplit("\n", 1)[-1]
+        text = ""
+        if command.startswith("[") and command.endswith("]"):
+            command = command[1:-1].strip()
+
+            if command == "next.":
+                for k, v in self.nextState.items():
+                    self.state[k] = v
+                self.nextState = dict()
+                for k in self.varNames:
+                    if text != "": text += ","
+                    if type(self.state[k]) is float:
+                        text += f" {k}={self.state[k]:g}"
+                    else:
+                        text += f" {k}={self.state[k]}"
+
+            elif command == "resetall.":
+                self.state = dict()
+                self.nextState = dict()
+                self.varNames = list()
+
+            elif m := re.match(r"(\w+)' *:= *(.*)", command):
+                newVar, expr = m[1], m[2]
+                expr = expr.replace(" ", "")
+                expr = re.findall(r"(?:\.?[0-9]+(?:\.[0-9]+)?(?:e[-+]?[0-9]+)?|[a-zA-Z_]+|.)", expr)
+                expr = [self.state.get(t, t) for t in expr if t is not None and t != ""]
+                text = f" {''.join([f'{v:g}' if type(v) is float else v for v in expr]).replace(' ','')}"
+                while expr != (newExpr := evalExprStep(expr)):
+                    expr = newExpr
+                    text += f"={''.join([f'{v:g}' if type(v) is float else v for v in expr]).replace(' ','')}"
+                if newVar not in self.state and newVar not in self.nextState:
+                    self.varNames.append(newVar)
+                assert len(expr) == 1
+                self.nextState[newVar] = expr[0]
+
+            elif m := re.match(r"(.*)\.$", command):
+                expr = m[1].replace(" ", "")
+                expr = re.findall(r"(?:\.?[0-9]+(?:\.[0-9]+)?(?:e[-+]?[0-9]+)?|[a-zA-Z_]+|.)", expr)
+                expr = [self.state.get(t, t) for t in expr if t is not None and t != ""]
+                text = f" {''.join([f'{v:g}' if type(v) is float else v for v in expr]).replace(' ','')}"
+                while expr != (newExpr := evalExprStep(expr)):
+                    expr = newExpr
+                    text += f"={''.join([f'{v:g}' if type(v) is float else v for v in expr]).replace(' ','')}"
+
+            else:
+                text = " ???"
+            text += "\n"
+        return self.appendText(text)
 
     def __repr__(self):
         return styleMathGPT(f"--MathGPT-BEGIN--\n{lines}\n--MathGPT-END--")
@@ -108,8 +221,12 @@ def readMathGptFile(filename):
 
 
 @click.group()
-def cli(): pass
+def cli():
+    """MathGPT command line interface
 
+    Example:
+    python3 mathgpt.py query "Alice has ten apples. Bob has seven apples. Alice trades half of her apples with Bob. Bob later eats a third of his apples. How many apples has Bob left?"
+    """
 
 @cli.command()
 @click.argument('files', nargs=-1)
@@ -168,16 +285,17 @@ One-Line Summary:
 
     title = complete(model="text-davinci-003", prompt=txt, temperature=0, max_tokens=20, stop="\n")
 
-    txt = f"""# {title}\n\n## Problem Statement\n{prompt}\n\n## Analysis\n"""
-    echo(styleMathGPT(txt), nl=False)
+    instance = MathGPT(f"""# {title}\n\n## Problem Statement\n{prompt}\n\n## Analysis\n""")
+    echo(styleMathGPT(instance.text), nl=False)
 
     while True:
-        response = complete(model=tuned_model, prompt=txt, temperature=0, max_tokens=200, stop=(" ]\n", " ] "))
+        response = complete(model=tuned_model, prompt=instance.text, temperature=0, max_tokens=200, stop=(" ]\n", " ] "))
 
         completion = response + " ]"
-        echo(styleMathGPT(completion))
+        instance.appendText(completion)
+        echo(styleMathGPT(completion), nl=False)
+        echo(styleMathGPT(instance.appendOutput()), nl=False)
 
-        txt += f"{completion}\n"
         if completion.endswith("[ resetall. ]"): break
 
     echo()
