b0443459abd0aa6b3b9f5ff2382e82234eab4605
Claire Xenia Wolf <claire@clairexen.net>
2023-12-27 12:34:43 +0100

    ...
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- asan-initfiasc-demo/run.sh
+++ asan-initfiasc-demo/run.sh
@@ -2,8 +2,9 @@
 set -ev
 CXX="clang++"
 CXXFLAGS="-g -fsanitize=address"
+#CXXFLAGS="${CXXFLAGS} -std=c++17"     # ./pass fails with this
+CXXFLAGS="${CXXFLAGS} -std=c++20"      # ./pass fails without this
 CXXFLAGS="${CXXFLAGS} -stdlib=libc++"  # ./pass fails without this
-CXXFLAGS="${CXXFLAGS} -std=c++2c"      # ./pass fails without this
 ${CXX} ${CXXFLAGS} -D ASAN_INIT_ORDER_EXPECT_PASS -g -o pass part1.cc part2.cc
 ${CXX} ${CXXFLAGS} -D ASAN_INIT_ORDER_EXPECT_FAIL -g -o fail part1.cc part2.cc
 ASAN_OPTIONS=check_initialization_order=true ./pass || { echo 'Failed ./pass !!!'; exit 1; }
