c120ce0ca327887920569266e1525f05a64ddb55
Claire Xenia Wolf <claire@clairexen.net>
2023-12-28 18:07:17 +0100

    ...
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- asan-initfiasc-demo/part2.cc
+++ asan-initfiasc-demo/part2.cc
@@ -1,10 +1,14 @@
 #include <vector>
 extern std::vector<int> extern_int_vec;
 
+#ifdef ASAN_INIT_ORDER_EXPECT_FAIL
+std::vector<int> extern_int_vec{1,2,3};
+#endif
+
 #ifdef ASAN_INIT_ORDER_EXPECT_PASS
 std::vector<int> extern_int_vec;
 #endif
 
-#ifdef ASAN_INIT_ORDER_EXPECT_FAIL
-std::vector<int> extern_int_vec{1,2,3};
+#ifdef ASAN_INIT_ORDER_CONSTINIT
+constinit std::vector<int> extern_int_vec;
 #endif
--- asan-initfiasc-demo/run.sh
+++ asan-initfiasc-demo/run.sh
@@ -5,8 +5,16 @@ CXXFLAGS="-g -fsanitize=address"
 #CXXFLAGS="${CXXFLAGS} -std=c++17"     # ./pass fails with this
 CXXFLAGS="${CXXFLAGS} -std=c++20"      # ./pass fails without this
 CXXFLAGS="${CXXFLAGS} -stdlib=libc++"  # ./pass fails without this
-${CXX} ${CXXFLAGS} -D ASAN_INIT_ORDER_EXPECT_PASS -g -o pass part1.cc part2.cc
+${CXX} ${CXXFLAGS} -D ASAN_INIT_ORDER_EXPECT_FAIL -g -S -o fail_part2.s part2.cc
+${CXX} ${CXXFLAGS} -D ASAN_INIT_ORDER_EXPECT_PASS -g -S -o pass_part2.s part2.cc
+${CXX} ${CXXFLAGS} -D ASAN_INIT_ORDER_CONSTINIT -g -S -o constinit_part2.s part2.cc
 ${CXX} ${CXXFLAGS} -D ASAN_INIT_ORDER_EXPECT_FAIL -g -o fail part1.cc part2.cc
-ASAN_OPTIONS=check_initialization_order=true ./pass || { echo 'Failed ./pass !!!'; exit 1; }
-ASAN_OPTIONS=check_initialization_order=true ./fail && { echo 'Passed ./fail !!!'; exit 1; }
+${CXX} ${CXXFLAGS} -D ASAN_INIT_ORDER_EXPECT_PASS -g -o pass part1.cc part2.cc
+${CXX} ${CXXFLAGS} -D ASAN_INIT_ORDER_CONSTINIT -g -o constinit part1.cc part2.cc
+ASAN_OPTIONS=print_legend=false:check_initialization_order=true ./fail && { echo 'Passed ./fail !!!'; exit 1; }
+ASAN_OPTIONS=check_initialization_order=true ./pass || { echo 'Failed check_initialization_order ./pass !!!'; exit 1; }
+ASAN_OPTIONS=check_initialization_order=true ./constinit || { echo 'Failed check_initialization_order ./constinit !!!'; exit 1; }
+ASAN_OPTIONS=strict_init_order=true ./pass || { echo 'Failed strict_init_order ./pass !!!'; exit 1; }
+ASAN_OPTIONS=strict_init_order=true ./constinit || { echo 'Failed strict_init_order ./constinit !!!'; exit 1; }
+! diff -u pass_part2.s constinit_part2.s
 echo OKAY
