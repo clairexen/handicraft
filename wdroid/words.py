1dd6671bc9fdd4f4318917c3c561cafa467b1f99
Claire Xenia Wolf <claire@clairexen.net>
2025-01-25 11:06:52 +0100

    ...
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- wdroid/words.py
+++ wdroid/words.py
@@ -1,13 +1,14 @@
 import nltk
 from nltk.corpus import wordnet, words
-nltk.download('wordnet')
+
 nltk.download('words')
+all_en_words = set(words.words('en'))
+
+nltk.download('wordnet')
+all_wordnet_lemmas = set(wordnet.all_lemma_names())
 
 dictfile = "/usr/share/dict/american-english-insane"
 #dictfile = "/usr/share/dict/american-english-large"
-
-all_en_words = set(words.words('en'))
-all_wordnet_lemmas = set(wordnet.all_lemma_names())
 with open(dictfile) as f:
     all_dict_words = {w.strip() for w in f}
 
@@ -17,6 +18,7 @@ with open("words.cc", "w") as f:
     for N in (4, 5, 6):
         words = "".join(sorted([w for w in all_words if len(w) == N and
                                 w.isalpha() and w.islower() and w.isascii()]))
+        print(f"extern const char WordleDroidWords{N}[];", end="", file=f)
         print(f"const char WordleDroidWords{N}[] = // {len(words)//N} words", end="", file=f)
         for k in range(0, len(words), 60):
             print(f"\n\"{words[k:k+60]}\"", end="", file=f)
