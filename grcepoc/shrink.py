e31ad5c68eae8488f2aced59a443fcbd424d4094
Claire Xenia Wolf <claire@clairexen.net>
2026-01-21 19:48:01 +0100

    ...
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- grcepoc/shrink.py
+++ grcepoc/shrink.py
@@ -12,8 +12,8 @@ import re
 
 import matplotlib.pyplot as plt
 
-STOP_WORDS = {"county", "census", "municipalit", "united states", "is a commune"}
-
+STOP_WORDS = {"county", "census", "municipalit", "united states", "a comm?une",
+              "a member", "member of", "american", "the first", "list of", "type of"}
 
 def collect_word_counts(
     lines: list[str], *, word_re: re.Pattern[str]
