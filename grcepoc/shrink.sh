da78a9aef583b39e7428c100fb81695691ef6547
Claire Xenia Wolf <claire@clairexen.net>
2026-01-22 02:07:46 +0100

    add pre-generated tokenizer
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- grcepoc/shrink.sh
+++ grcepoc/shrink.sh
@@ -13,6 +13,11 @@ gzip --rsyncable -9 < shrink-test.txt > data/simplerwiki-test.txt.gz
 for x in 1 2 3; do python shrink.py --test; done
 gzip --rsyncable -9 < shrink-test.txt > data/simplestwiki-test.txt.gz
 
-rm -vf data/simple{,r,st}wiki_tokens_{test,train}_2000.pt
 rm -f shrink-{train,test}.{txt,png}
+rm -vf data/simple{r,st}wiki_tokens_{test,train}_2000.pt
+
+# rm -vf data/simple{r,st}wiki_vocab_2000.json
+# python grce.py corpus --init-tokenizer
+# cp -v data/simple{r,st}wiki_vocab_2000.json
+
 exit 0
