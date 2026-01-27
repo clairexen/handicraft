2f792ee4b7ca8036fa2f01a5c6c5f73998aa15a1
Claire Xenia Wolf <claire@clairexen.net>
2026-01-27 07:25:17 +0100

    fix log issue
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- grcepoc/grce.py
+++ grcepoc/grce.py
@@ -1698,10 +1698,9 @@ class GPT2TokenizerWrapper:
             s = self.decode_one(tok)
             assert s, "got empty token"
             s = s.replace("\n", FANCY_ENTER if settings.escape_newline_tokens else FANCY_ENTER.replace(" ", "\n"))
-            if s == " ":
-                s = FANCY_SPACE
-            else:
-                parts.append(color + s + Colors.RESET)
+            if not s.replace(" ", ""):
+                s = s.replace(" ", FANCY_SPACE)
+            parts.append(color + s + Colors.RESET)
             color, altcolor = altcolor, color
         return "".join(parts)
 
