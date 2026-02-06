cd8bec154a6dd24403e43710114a6a53f3373dd9
Claire Xenia Wolf <claire@clairexen.net>
2026-02-06 01:58:10 +0100

    update
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- grcepoc/data/cccclc-setup.sh
+++ grcepoc/data/cccclc-setup.sh
@@ -6,7 +6,6 @@ volume=${1:-0000}
 if [ ! -f dolma-cccc-filtered-${volume}.json.gz ]; then
 	# https://huggingface.co/datasets/common-pile/cccc_filtered/tree/main
 	wget "https://huggingface.co/datasets/common-pile/cccc_filtered/resolve/main/dolma-cccc-filtered-${volume}.json.gz"
-	exit 1
 fi
 
 if [ ! -f cccclc-${volume}-train.txt.gz ]; then
@@ -14,6 +13,6 @@ if [ ! -f cccclc-${volume}-train.txt.gz ]; then
 fi
 
 if [ ! -f cccclc-${volume}_tokens_train_5000.pt ]; then
-	../.venv/bin/python python corpus.py tokens --tokenizer cccclc_vocab_5000.json \
-			--input cccclc-${volume}-test.txt.gz --output data/cccclc-${volume}_tokens_test_5000.pt
+	../.venv/bin/python ../corpus.py tokens --tokenizer cccclc_vocab_5000.json \
+			--input cccclc-${volume}-test.txt.gz --output cccclc-${volume}_tokens_test_5000.pt
 fi
